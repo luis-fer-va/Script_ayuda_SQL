52a2da2b0551147241e51adc17f0ab29272959fb
luis-fer-va <84397763+luis-fer-va@users.noreply.github.com>
2023-05-26 19:10:34 -0500

    Update Actualizar varios registros.sql

--- Actualizar varios registros.sql
+++ Actualizar varios registros.sql
@@ -1,3 +1,17 @@
+-- 1. Crear una tabla tmp, que contendra los registros unicos y los registros a mapear.
+create table map_product_line(
+	map nvarchar(50)not null,product nvarchar(50)not null);
+  
+-- 2. Insertar los datos 
+insert into map_product_line values(
+(Trains,Trenes),
+('Motorcycles','Motocicletas'),
+('Ships','Barcos'),
+('Trucks and Buses','Camiones y Autobuses'),
+('Vintage Cars','Automóviles Antiguos'),
+('Classic Cars','Automóviles Clásicos');
+ 
+-- 3. Actualizar los datos
 update [sales_data_sample]
 set STATUS=c.tbl1
 from [sales_data_sample] v
